eb32b62c702f516f357edc698cbceb9345138725
Akram Kadri <ak224hz@student.lnu.se>
2021-06-03 12:25:53 +0100

    Redesign writing-upload page with  Material-ui

--- src/components/pages/UploadWriting.js
+++ src/components/pages/UploadWriting.js
@@ -2,14 +2,44 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import { Redirect } from 'react-router'
 import { isLoggedIn } from '../../helper.js'
+import Typography from '@material-ui/core/Typography'
+import { Button } from '@material-ui/core'
+import { Link as RouterLink } from 'react-router-dom'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
 
 
 const apiURL = (process.env.NODE_ENV === 'production') ? 'https://cscloud7-201.lnu.se/api' : 'http://localhost:7003'
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(6, 2)
+  },
+  form: {
+
+  },
+  titleGroup: {
+    marginTop: theme.spacing(1)
+  },
+  checkboxGroup: {
+    marginTop: theme.spacing(1)
+  },
+  button: {
+    width: '120px',
+    marginTop: theme.spacing(1)
+  }
+}));
+
 export default function UploadWriting() {
+  const classes = useStyles()
+
   const [text, setText] = useState('')
   const [title, setTitle] = useState('')
-  const [active, setActive] = useState(true)
+  const [active, setActive] = useState(false)
 
   const submitHandler = async (e) => {
     e.preventDefault()
@@ -23,7 +53,6 @@ export default function UploadWriting() {
       title: title,
       text: text,
       active: active
-
     };
 
     try {
@@ -44,53 +73,53 @@ export default function UploadWriting() {
   }
 
   return isLoggedIn() ? (
-    <div className="sm:container mx-auto px-4 pt-8 flex flex-col sm:flex-row justify-center">
-      <div id="text-group" className="w-full mb-4 lg:max-w-2xl flex flex-col">
-        <label for="textarea" className="text-4xl mb-4 text-center">Enter your text</label>
-        <textarea
-          id="textarea"
-          onChange={(e) => setText(e.target.value)}
-          className="rounded border-2 w-full h-96 focus:border-primary focus:ring-primary">
-        </textarea>
-      </div>
+      <Container maxWidth='md' className={classes.root}>
+        <TextField
+          label='Your text'
+          variant='outlined'
+          multiline
+          rows={16}
+          margin="normal"
+          required
+          fullWidth
+          autoFocus={true}
+          value={text}
+          onChange={(e) => setText(e.target.value)}/>
 
-      <div id="menu" className="sm:ml-6 sm:pt-14 sm:mt-0 font-mono">
+        <div className={classes.form}>
 
-        <div id="title-group" className="block">
-          <label htmlFor="categories" className="block">Title/Type</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="inline-block max-w-full border-2 rounded focus:border-primary focus:ring-primary" />
+          <div className={classes.titleGroup}>
+            <Typography variant='subtitle1'>Title/Type</Typography>
+            <TextField
+              variant='outlined'
+              label='Title'
+              required
+              onChange={(e) => setTitle(e.target.value)}/>
+          </div>
+
+          <FormGroup row className={classes.checkboxGroup}>
+            <FormControlLabel
+              label='Test active'
+              control={
+                <Checkbox
+                  checked={active}
+                  color='primary'
+                  onChange={(e) => setActive(e.target.checked)}
+                />
+              }
+            />
+          </FormGroup>
+
+          <Button variant='contained'
+                  color='primary'
+                  component={RouterLink}
+                  to='/register'
+                  className={classes.button}
+                  onClick={submitHandler}>Submit
+          </Button>
+
         </div>
-
-        {/* <div id="select-menu" className="block mt-3">
-            <label htmlFor="categories" className="block">Choose a category:</label>
-            <select
-              id="categories"
-              onChange={(e) => setCategory(e.target.value)}
-              className="block border-2 pl-5 pr-8 py-3 rounded-full focus:border-primary focus:ring-primary">
-              <option value="general">General</option>
-              <option value="personal-letter">Personal letter</option>
-              <option value="cv">CV</option>
-            </select>
-          </div> */}
-
-        <div id="checkbox-group" className="block space-x-2 mt-3">
-          <span className="inline-block">Set test active</span>
-          <input
-            type="checkbox"
-            checked={active}
-            onChange={(e) => setActive(e.target.checked)}
-            className="inline-block rounded text-primary focus:border-primary focus:ring-primary" />
-        </div>
-
-        <button onClick={submitHandler} className="block bg-primary text-white rounded-full focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 py-2 px-10 mt-6">Submit</button>
-
-      </div>
-    </div>
-
+      </Container>
   ) : (
       <Redirect to="/" />
     )
